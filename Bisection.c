16b4cf1c379c122bea713eb49c500d56b3459879
saimon4u <bsse1402@iit.du.ac.bd>
2023-08-02 20:40:23 +0600

    changes

--- Bisection.c
+++ Bisection.c
@@ -1,26 +1,23 @@
 #include<stdio.h>
 #include<math.h>
 #define E 1e-6
-double calculate(double x){
-    return (x*x*x) + (4*x*x) - 10;
-}
-
+#define f(x) (x*x*x + 4*x*x - 10)
 
 void bisection(double a,double b){
-	printf("No:	a		b		c		f(a)		f(b)		f(c)\n");
+	printf("No:	a		b		mid		f(a)		f(b)		f(mid)\n");
     int i = 1;
     while(1){
-        if((calculate(a)*calculate(b)) >= 0){
+        if((f(a)*f(b)) >= 0){
             printf("Solution doesn't exists!\n");
             break;
         }
         double mid = (a+b)/2;
-        printf("%d	%lf	%lf	%lf	%lf	%lf	%lf\n",i,a,b,mid,calculate(a),calculate(b),calculate(mid));
-        if(fabs(calculate(mid)) < E){
+        printf("%d	%lf	%lf	%lf	%lf	%lf	%lf\n",i,a,b,mid,f(a),f(b),f(mid));
+        if(fabs(f(mid)) < E){
             printf("The root of this equation is: %lf\n",mid);
             break;
         }
-        if((calculate(a)*calculate(mid)) < 0)b = mid;
+        if((f(a)*f(mid)) < 0)b = mid;
         else a = mid;
     	i++;
     }
